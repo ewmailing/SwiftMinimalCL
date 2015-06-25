f72186826eb1c273669e048aeef4e938121420af
Eric Wing <ewing -dot- public |-at-| gmail.com>
2015-06-25 05:41:15 -0700

    Initial Commit

---
+++ MinimalSwiftCL/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MinimalSwiftCL
+//
+//  Created by Eric Wing on 6/25/15.
+//  Copyright (c) 2015 PlayControl Software LLC. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
